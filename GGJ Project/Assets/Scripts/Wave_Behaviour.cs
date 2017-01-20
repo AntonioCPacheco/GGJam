0ce4dc6701d2833fd610aa213866c587abce708c
AntonioCPacheco <cpacheco94@gmail.com>
2017-01-20 22:35:25 +0000

    Waves first commit

---
+++ GGJ Project/Assets/Scripts/Wave_Behaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave_Behaviour : MonoBehaviour {
+
+    public float minScale = 1;
+    public float maxScale = 4;
+    public int timeToSpread = 3;
+    float ratioOfIncrease = 0;
+    float timeOfCreation = 0;
+
+	// Use this for initialization
+	void Start () {
+        transform.localScale = new Vector3(minScale, minScale, 1);
+        timeOfCreation = Time.realtimeSinceStartup;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.localScale.x >= maxScale)
+        {
+            GameObject.Destroy(this.gameObject);
+        } else
+        {
+            float ratio = ((maxScale - minScale) * (Time.realtimeSinceStartup - timeOfCreation) / timeToSpread);
+            transform.localScale = new Vector3(transform.localScale.x + ratio, transform.localScale.y + ratio, 1);
+        }
+	}
+}
