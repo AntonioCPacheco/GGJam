993471896b4e19ecb27214d1d97264718ed2772d
Baltazar95 <tiago_mpb95@hotmail.com>
2017-01-21 11:41:38 +0000

    Repeater object added

--- GGJ Project/Assets/Scripts/ReflectionObject.cs
+++ GGJ Project/Assets/Scripts/ReflectionObject.cs
@@ -2,26 +2,7 @@
 using System;
 
 public class ReflectionObject : InteractableObject
-{/*
-    public EdgeCollider2D self_collider;
-    public Vector2 collider_orientation;
-
-    public ReflectionObject()
-    {
-        Vector2 pos1 = self_collider.points[0];
-        Vector2 pos2 = self_collider.points[self_collider.points.Length - 1];
-
-        collider_orientation = pos2 - pos1;
-    }
-    
-    public void Collided(RaycastHit2D ray, Vector2 origin)
-    {
-        Vector2 point_of_collision = ray.point;
-        Vector2 hit_vector = point_of_collision - origin;
-
-        //Physics2D.Linecast(point_of_collision, );
-    }*/
-
+{
     public override void trigger(RaycastHit2D col, Vector2 origin, float radius)
     {/*
         Vector2 numberOne = new Vector2(-1,-1);
---
+++ GGJ Project/Assets/Scripts/RepeaterObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+
+public class RepeaterObject : InteractableObject
+{
+    public CircleCollider2D circle;
+
+    void Start()
+    {
+        circle = this.gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    public override void trigger(RaycastHit2D col, Vector2 origin, float radius)
+    {
+        float aux_vec_x = col.point.x;
+        float aux_vec_y = col.point.y;
+        Vector2 aux_vec = new Vector2(aux_vec_x, aux_vec_y);
+        Vector2 new_origin = aux_vec.normalized * circle.radius + (Vector2)transform.position;
+        Debug.Log(new_origin);
+        Physics2D.Linecast(new_origin, (new_origin + aux_vec.normalized)*radius);
+        Debug.DrawLine(new_origin, (new_origin + aux_vec.normalized) * radius, Color.white);
+        /*
+        Vector2 center = circle.transform.position;
+        float xx = 
+
+        Vector2 aux_vec = circle.offset - col.point;
+        Vector2 new_origin = (Vector2)circle.transform.position - aux_vec;
+        Debug.Log(circle.transform.position);
+        Physics2D.Linecast(new_origin, ((origin - col.point).normalized));
+        Debug.DrawLine(new_origin, ((origin - col.point).normalized), Color.white);
+        */
+    }
+}
