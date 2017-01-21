7458bef63b319a482f7c90b488eb1329c3ee52e6
AntonioCPacheco <cpacheco94@gmail.com>
2017-01-21 01:58:22 +0000

    Waves with LineCasts

--- GGJ Project/Assets/Scripts/Wave_Behaviour.cs
+++ GGJ Project/Assets/Scripts/Wave_Behaviour.cs
@@ -3,20 +3,52 @@ using System.Collections;
 
 public class Wave_Behaviour : MonoBehaviour {
 
-    public float minScale = 1;
-    public float maxScale = 4;
-    public int timeToSpread = 3;
-    float ratioOfIncrease = 0;
-    float timeOfCreation = 0;
+    public Vector2 direction;
+    public int mode = 0; //0 - omnidirectional | 1 - directional
+    public float radius = 1.5f;
+    float currentRadius = 0;
 
 	// Use this for initialization
 	void Start () {
-        transform.localScale = new Vector3(minScale, minScale, 1);
-        timeOfCreation = Time.realtimeSinceStartup;
+        if(mode == 1)
+            direction.Normalize();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        currentRadius += 0.1f;
+        switch (mode)
+        {
+            case (0):
+                for(int i = 0; i < 64; i++)
+                {
+                    Vector2 endpoint = new Vector2(Mathf.Cos((2*Mathf.PI / 64)*i), Mathf.Sin((2 * Mathf.PI / 64) * i));
+                    RaycastHit2D rayhit = Physics2D.Linecast(transform.position, currentRadius * endpoint.normalized, LayerMask.NameToLayer("WaveCollision"));
+                    if(rayhit)
+                    {
+                        Debug.Log("HIT SOMEHTING! It's name is: " + rayhit.collider.gameObject.name);
+                        //FIXME
+                        //rayhit.collider.gameObject.GetComponent<>().trigger(transform.position, rayhit);
+                    }
+                }
+                break;
+            case (1):
+                for(int i = 0; i < 8; i++)
+                {
+                    Vector2 endpoint = new Vector2(Mathf.Cos((2 * Mathf.PI / 64) * i), Mathf.Sin((2 * Mathf.PI / 64) * i));
+                    RaycastHit2D rayhit = Physics2D.Linecast(transform.position, currentRadius * endpoint.normalized, LayerMask.NameToLayer("WaveCollision"));
+                    if (rayhit)
+                    {
+                        Debug.Log("HIT SOMEHTING! It's name is: " + rayhit.collider.gameObject.name);
+                        //FIXME
+                        //rayhit.collider.gameObject.GetComponent<>().trigger(transform.position, rayhit);
+                    }
+                }
+                break;
+        }
+        
+        
+        /*
         if(transform.localScale.x >= maxScale)
         {
             GameObject.Destroy(this.gameObject);
@@ -24,6 +56,6 @@ public class Wave_Behaviour : MonoBehaviour {
         {
             float ratio = ((maxScale - minScale) * (Time.realtimeSinceStartup - timeOfCreation) / timeToSpread);
             transform.localScale = new Vector3(transform.localScale.x + ratio, transform.localScale.y + ratio, 1);
-        }
+        }*/
 	}
 }
