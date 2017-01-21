93586cc393c21b11563cd6a7d0cf96d6226df788
Baltazar95 <tiago_mpb95@hotmail.com>
2017-01-21 21:20:54 +0000

    added doors, switchs and corrected some errors

---
+++ Assets/Scripts/WaveAbsorber.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System;
+
+public class WaveAbsorber : InteractableObject
+{
+    public override void trigger(RaycastHit2D col, Vector2 origin, float radius)
+    {
+
+    }
+}
