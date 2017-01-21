7c672cc40dda40f02effaf5e3a807c0b4aad429a
AntonioCPacheco <cpacheco94@gmail.com>
2017-01-21 21:01:06 +0000

    Reflexions finallly done

--- Assets/Scripts/RepeaterObject.cs
+++ Assets/Scripts/RepeaterObject.cs
@@ -33,24 +33,5 @@ public class RepeaterObject : InteractableObject
         wave.gameObject.transform.position = col.point;
         wave.direction = dir;
         wave.radius = 10;
-
-        /*
-        float aux_vec_x = col.point.x;
-        float aux_vec_y = col.point.y;
-        Vector2 aux_vec = new Vector2(aux_vec_x, aux_vec_y);
-        Vector2 new_origin = aux_vec.normalized * circle.radius + (Vector2)transform.position;
-        Debug.Log(new_origin);
-        Physics2D.Linecast(new_origin, (new_origin + aux_vec.normalized)*radius);
-        Debug.DrawLine(new_origin, (new_origin + aux_vec.normalized) * radius, Color.white);
-        /*
-        Vector2 center = circle.transform.position;
-        float xx = 
-
-        Vector2 aux_vec = circle.offset - col.point;
-        Vector2 new_origin = (Vector2)circle.transform.position - aux_vec;
-        Debug.Log(circle.transform.position);
-        Physics2D.Linecast(new_origin, ((origin - col.point).normalized));
-        Debug.DrawLine(new_origin, ((origin - col.point).normalized), Color.white);
-        */
     }
 }
