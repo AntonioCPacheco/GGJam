40f85c28cac27c42ae697f5ec9c670bdb010e225
zuchis <liquimaster@gmail.com>
2017-01-21 20:20:43 +0000

    added doors, door animations and door triggers

---
+++ Assets/Scripts/Door_Switch_Behaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_Switch_Behaviour : MonoBehaviour {
+
+    public Vector3 doorFinalPos;
+    public bool animationStart = false;
+
+    Vector3 doorInitialPos;
+    float transitionStatus = .0f;
+    float transitionDuration = 2.0f;
+
+	void Start () {
+        doorInitialPos = transform.position;
+	}
+	
+	void Update () {
+        if (animationStart == true)
+        {
+            if (transitionStatus < 1.0f)
+            {
+                transitionStatus += (1.0f / transitionDuration) * Time.deltaTime;
+                transform.position = Vector3.Lerp(doorInitialPos, doorFinalPos, transitionStatus);
+            }
+        }
+	
+	}
+}
---
+++ Assets/Scripts/Switch_Trigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch_Trigger: InteractableObject {
+
+    public GameObject door;
+
+    void Start()
+    {
+        
+    }
+
+    public override void trigger(RaycastHit2D col, Vector2 origin, float radius)
+    {
+        door.GetComponent<Door_Switch_Behaviour>().animationStart = true;
+
+    }
+}
