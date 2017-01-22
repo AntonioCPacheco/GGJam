43263c1aa412ca3b7b7d3d9f6782cec21781716d
zuchis <liquimaster@gmail.com>
2017-01-22 03:55:51 +0000

    scripts updates

--- Assets/Scripts/Player_Attack.cs
+++ Assets/Scripts/Player_Attack.cs
@@ -7,6 +7,7 @@ public class Player_Attack : MonoBehaviour
     public float maxRadius = .5f;
     public float castSpeed = .01f;
     public float dragFriction = 50.0f;
+    public float deltaThreshold = 1f;
 
     private Vector2 direction;
     private float currentRadius = 0.0f;
@@ -50,13 +51,15 @@ public class Player_Attack : MonoBehaviour
             //    currentRadius = Mathf.Clamp(currentRadius + castSpeed, 0.0f, maxRadius);
 
             Debug.DrawLine(transform.position, mouseInput, Color.red);
-            if (rayhit.collider != null)
+            if (rayhit.collider != null && rayhit.collider.tag == "Pickable")
             {
                 rayhit.rigidbody.velocity = Vector2.zero;
 
                 rayhit.transform.position = new Vector3(mouseInput.x, mouseInput.y, 0.0f); //new Vector3(this.transform.position.x + currentRadius, this.transform.position.y + currentRadius);
-                rayhit.rigidbody.velocity = new Vector2(delta.x / dragFriction, delta.y / dragFriction);
+                if (!mouseInput.Equals(delta))
+                    rayhit.rigidbody.velocity = new Vector2(delta.x / dragFriction, delta.y / dragFriction);
             }
+            lastPos = Input.mousePosition;
         }
         else
         {
