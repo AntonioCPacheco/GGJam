85f328dc420a5d540d2aaae5205093a01101fa15
Baltazar95 <tiago_mpb95@hotmail.com>
2017-01-22 12:14:23 +0000

    new prefab and some corrections

---
+++ Assets/Scripts/Energy_Fountain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Energy_Fountain : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<Player_Life>().life = 4;
+            col.gameObject.GetComponent<SpriteRenderer>().sprite = col.gameObject.GetComponent<Player_Life>().blobSprites[col.gameObject.GetComponent<Player_Life>().life - 1];
+        }
+        this.gameObject.SetActive(false);
+    }
+}
